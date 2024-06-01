74f348f02913501a8b48e1c62bc7b7eb63406d07
Brian Maitho <Bmaitho@gmail.com>
2024-06-01 14:41:04 +0300

    All tests passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+  def __init__(self, word):
+    self.word = word.lower()  
+
+  def is_anagram(self, word):
+    other_word = word.lower()  
+    return sorted(self.word) == sorted(other_word)
+
+  def match(self, words):
+    anagrams = []
+    for word in words:
+      if self.is_anagram(word) and word != self.word:  
+        anagrams.append(word)
+    return anagrams
+
